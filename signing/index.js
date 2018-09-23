af90880e4f75b96114a1a3a728ab4fd2bd298d9b
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-09-24 00:16:06 +0800

    feat(signing): added signature generation methods

--- signing/index.js
+++ signing/index.js
@@ -6,24 +6,38 @@ const eddsaEd25519 = new elliptic.eddsa('ed25519')
 
 const schemes = {
   'ecdsa-ed25519': {
+    create: (message, secret) => {
+      return ecdsaEd25519.sign(message, secret).toDER('hex')
+    },
     verify: (message, signature, publicKey, encoding = 'hex') => {
       return ecdsaEd25519.verify(message, signature, publicKey, encoding)
     }
   },
   'eddsa-ed25519': {
+    create: (message, secret) => {
+      return eddsaEd25519.sign(message, secret).toHex()
+    },
     verify: (message, signature, publicKey) => {
       return eddsaEd25519.verify(message, signature, publicKey)
     }
   }
 }
 
-function verify({scheme, message, signature, publicKey, encoding}) {
+function create({ scheme, message, secret } = {}) {
   let someScheme = schemes[scheme]
-  if (!someScheme) return false
+  if (!someScheme) return {error: 'invalid-signature-scheme'}
+
+  return someScheme.create(message, secret)
+}
+
+function verify({ scheme, message, signature, publicKey, encoding } = {}) {
+  let someScheme = schemes[scheme]
+  if (!someScheme) return {error: 'invalid-signature-scheme'}
 
   return someScheme.verify(message, signature, publicKey, encoding)
 }
 
 module.exports = {
+  create,
   verify
 }
