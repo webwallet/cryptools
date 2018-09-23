3d88b4fa3dfa24112f1359b60605ce401b8eb056
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-09-23 22:17:11 +0800

    feat(signing): added eddsa-ed25519 signature verification

--- signing/index.js
+++ signing/index.js
@@ -1,21 +1,27 @@
 'use strict'
 
 const elliptic = require('elliptic')
-const ed25519 = new elliptic.ec('ed25519')
+const ecdsaEd25519 = new elliptic.ec('ed25519')
+const eddsaEd25519 = new elliptic.eddsa('ed25519')
 
 const schemes = {
-  ed25519: {
-    verify: (hash, signature, publicKey, encoding = 'hex') => {
-      return ed25519.verify(hash, signature, publicKey, encoding)
+  'ecdsa-ed25519': {
+    verify: (message, signature, publicKey, encoding = 'hex') => {
+      return ecdsaEd25519.verify(message, signature, publicKey, encoding)
+    }
+  },
+  'eddsa-ed25519': {
+    verify: (message, signature, publicKey) => {
+      return eddsaEd25519.verify(message, signature, publicKey)
     }
   }
 }
 
-function verify({message, signature, algorithm, publicKey, encoding}) {
-  let scheme = schemes[algorithm]
-  if (!scheme) return false
+function verify({scheme, message, signature, publicKey, encoding}) {
+  let someScheme = schemes[scheme]
+  if (!someScheme) return false
 
-  return scheme.verify(message, signature, publicKey, encoding)
+  return someScheme.verify(message, signature, publicKey, encoding)
 }
 
 module.exports = {
