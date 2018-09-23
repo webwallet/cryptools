1104d93e3020953431d14c76b8a540b1a6c6e021
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-09-24 01:22:35 +0800

    feat(crypto): ported keypair generation logic from SDK

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const keypair = require('./keypair')
 const hashing = require('./hashing')
 const signing = require('./signing')
 const puzzles = require('./puzzles')
 
 module.exports = {
+  keypair,
   hashing,
   signing,
   puzzles
